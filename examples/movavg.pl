74aee742013f58a78eaa678dd184edb9d09e649b
Vikas N Kumar <vikas@cpan.org>
2014-01-12 15:47:24 -0500

    Updating movavg.pl to really use PDLs to create the data instead of just rcols.

--- examples/movavg.pl
+++ examples/movavg.pl
@@ -8,6 +8,7 @@ use PDL;
 use PDL::NiceSlice;
 use PDL::Finance::TA ':Func';
 
+my $symbol = $ARGV[0] || 'YHOO';
 ## Let's test if the moving average function works first
 if ($ENV{DEBUG}) {
     my $a = sequence 50;
@@ -21,13 +22,15 @@ eval 'require Finance::QuoteHist' || die "Please install the recommended ".
 eval { require DateTime; require PDL::Graphics::PGPLOT::Window; };
 $PDL::doubleformat = "%.6lf";
 
-## download the stock price data for YHOO and save to a CSV file
+## download the stock price data for $symbol and save to a CSV file
 my $tmpdir = $ENV{TEMP} || $ENV{TMP} if $^O =~ /Win32|Cygwin/i;
 $tmpdir = $ENV{TMPDIR} || '/tmp' unless $^O =~ /Win32|Cygwin/i;
-my $path = File::Spec->catfile($tmpdir, 'YHOO.csv');
+my $path = File::Spec->catfile($tmpdir, "$symbol.csv");
+my @quotes = ();
+my $data;
 unless (-e $path) {
     my $fq = new Finance::QuoteHist(
-        symbols => [qw(YHOO)],
+        symbols => [ $symbol ],
         start_date => '1 year ago',
         end_date => 'today',
     );
@@ -43,14 +46,18 @@ unless (-e $path) {
                     time_zone => 'America/New_York',
                  )->epoch;
         say $fh "$epoch,$o,$h,$l,$c";
+        push @quotes, pdl($epoch, $o, $h, $l, $c);
     }
     $fq->clear_cache;
     close $fh;
     say "$path has some data for analysis";
+    $data = pdl(@quotes)->transpose;
+} else {
+    ## now read this back into a PDL using rcol
+    say "$path already present. loading it...";
+    $data = rcols $path, [], { COLSEP => ',', DEFTYPE => PDL::double };
 }
 
-## now read this back into a PDL using rcol
-my $data = rcols $path, [], { COLSEP => ',', DEFTYPE => PDL::double };
 my ($start) = $data(0, (0))->list;
 my $start_day = DateTime->from_epoch(epoch => $start)->ymd;
 $data(0:-1,(0)) -= $start;
@@ -73,8 +80,8 @@ $win->line($data(0 + 13 - 1:-1,(0)), $data(0:-1,(4))->movavg(13),
 # plot the 21-day moving average of the close price
 $win->line($data(0 + 21 - 1:-1,(0)), $data(0:-1,(4))->movavg(21),
             { COLOR => 'YELLOW'});
-$win->label_axes("Days since $start_day", 'Close Price', 'Moving Average of Close Prices');
-$win->legend(['YHOO', '5-day Mov. Avg.', '13-day Mov. Avg.', '21-day Mov. Avg.'],
+$win->label_axes("Days since $start_day", 'Close Price', 'Simple Moving Average of Close Prices');
+$win->legend([$symbol, '5-day SMA', '13-day SMA', '21-day SMA'],
     40, 40, { Colour => ['CYAN', 'MAGENTA', 'RED', 'YELLOW'], XPos => 5, YPos =>
     5, Width => 'Automatic'});
 $win->release;
