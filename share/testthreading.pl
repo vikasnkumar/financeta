e72d243e051835333b7e4071e35e572ad6a08669
Vikas N Kumar <vikas@cpan.org>
2014-09-18 17:43:04 -0400

    updating the test-threading script. we have found a way to not use thread_define

--- share/testthreading.pl
+++ share/testthreading.pl
@@ -2,18 +2,37 @@
 
 use strict;
 use warnings;
-use PDL::Lite;
-use PDL::Core;
-use PDL::Basic;
+use PDL;
 use PDL::NiceSlice;
 
-my $a = ones(100, 4) + sequence(100, 4) * 0.05;
-print $a, "\n";
 
-thread_define 'bsell(o(n);h(n);l(n);c(n); [o]z(n))', over {
-    $_[4] .= $_[3] > (($_[3] + $_[0] + $_[1] + $_[2]) / 4);
+thread_define 'test1(x(n);i(n);[o]z(n))', over {
+    my ($x, $i, $z) = @_;
+    $z .= ($x > $x->index($i)) * 3.14159;
 };
 
-my $b = ones(100);
-bsell($a(,(0)), $a(,(1)), $a(,(2)), $a(,(3)), $b);
+thread_define 'test2(x(n);[o]z(n))', over {
+    my ($x, $z) = @_;
+    my $i = xvals($x->dims) - 1;
+    $i = $i->setbadif($i < 0)->setbadtoval(0);
+    $z .= 3.14159 * ($x > $x->index($i));
+};
+
+my $a = randsym 20;
+print $a, "\n";
+
+# pre-generate the indexing and then send to function
+my $i = xvals($a->dims) - 1;
+$i = $i->setbadif($i < 0)->setbadtoval(0);
+print $a->index($i), "\n";
+my $b; test1($a, $i, $b = null);
 print $b, "\n";
+
+# have the function handle the indexing
+my $c; test2($a, $c = null);
+print $c, "\n";
+
+my $d = zeroes($a->dims);
+my $d_i =  $d->where($a > $a->index($i));
+$d_i .= 3.14159;
+print $d, "\n";
